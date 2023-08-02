5b05164c31bf52bb4f6d946c26f23c76909f2153
yeldynov <yeldynov@gmail.com>
2023-08-02 21:04:04 +0300

    + homepage

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+      <Link href='/' className='font-bold text-xl'>
+        MASSIMO
+      </Link>
+      <p>&copy; ALL RIGHTS RESERVED.</p>
+    </div>
+  );
+};
+
+export default Footer;
